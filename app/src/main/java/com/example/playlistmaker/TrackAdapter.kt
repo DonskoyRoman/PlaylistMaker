2f59ef252ded7ac4741d6afd9ee3f81c127cea84
sysek <syseka671@gmail.com>
2024-11-12 23:56:44 +0300

    Sprint 11 Second Commit

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackAdapter(private var tracks: List<Track>) :
     RecyclerView.Adapter<TrackAdapter.TrackViewHolder>() {
@@ -37,9 +38,14 @@ class TrackAdapter(private var tracks: List<Track>) :
             val seconds = (durationInMillis / 1000) % 60
             trackTimeMillis.text = String.format("%d:%02d", minutes, seconds)
 
+
+            val cornerRadius =
+                itemView.context.resources.getDimensionPixelSize(R.dimen.very_small_corner_radius)
+
             val imageUrl = track.artworkUrl100
             Glide.with(itemView.context)
                 .load(imageUrl)
+                .transform(RoundedCorners(cornerRadius))
                 .placeholder(R.drawable.placeholder)
                 .error(R.drawable.placeholder)
                 .into(artworkImage)
